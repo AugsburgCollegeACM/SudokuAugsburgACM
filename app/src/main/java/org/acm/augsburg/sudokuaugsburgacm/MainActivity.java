b31826abd1fea9c5f7e5daa7504eb523f48ebe8d
DESKTOP-AB890R1\Peter <peterklein1923@gmail.com>
2017-01-25 15:32:47 -0600

    Initial commit

---
+++ app/src/main/java/org/acm/augsburg/sudokuaugsburgacm/MainActivity.java
@@ -0,0 +1,13 @@
+package org.acm.augsburg.sudokuaugsburgacm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
